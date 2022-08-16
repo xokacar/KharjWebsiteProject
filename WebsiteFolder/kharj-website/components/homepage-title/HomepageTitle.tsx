37d419eccbf121483c97ebe54b0151720128811d
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-08-16 21:08:22 +0300

    #kjWEB-02/Homepage title added

---
+++ WebsiteFolder/kharj-website/components/homepage-title/HomepageTitle.tsx
@@ -0,0 +1,16 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+export const HomepageTitle = () => {
+    return (
+        <Container >
+            <Box>
+                <Heading as="h1" size="lg" mb={4}>
+                    Kharj 
+                </Heading>
+    
+                
+            </Box>
+        </Container>
+
+    )
+}
