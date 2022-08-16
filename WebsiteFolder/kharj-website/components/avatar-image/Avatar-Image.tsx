9104a25c70ba4db93eaad2b5d4f9668823270a70
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-08-16 22:57:44 +0300

    #kjWEB-02/Avatar Image alt text added

--- WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
+++ WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
@@ -1,14 +1,14 @@
-import {Box, AspectRatio, Flex, Img} from '@chakra-ui/react'
+import {Box,AspectRatio, Flex, Img} from '@chakra-ui/react'
 
 export const AvatarImage = () => {
     return (
         
-            
                 <Box>
                     <Img
                     borderRadius='full'
                     boxSize='200px'
                     src='https://user-images.githubusercontent.com/44809357/184952777-451f8e3d-f73b-4474-97be-d604b5accf67.jpg'
+                    alt='Kharj Memoji Photo'
                     />     
                 </Box>
             
