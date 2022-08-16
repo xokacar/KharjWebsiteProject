e81c02f8197ff7ad162ba391e3dd10075b070a4a
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-08-17 00:04:10 +0300

    #kjWEB-02/HomepageTitle name updated as HomepageContent

---
+++ WebsiteFolder/kharj-website/components/homepage-content/HomepageContent.tsx
@@ -0,0 +1,22 @@
+import { Container, Box, Heading, Text } from '@chakra-ui/react'
+import { AvatarImage } from '../avatar-image/Avatar-Image'
+
+export const HomepageContent = () => {
+    return (
+
+        <Container >
+            <Box >
+                <Heading as="h1" size="lg" mb={4}>
+                    Kharj 
+                </Heading>
+                <AvatarImage/>
+                <Text as="h2" lineHeight='160%'>
+                    Hey I'm Kharj and I'm a software engineer.
+                     I'm currently working at a startup called  
+                       as a frontend engineer. I'm also a student at the University of Turkish Aeronautical Association studying Computer Engineer.
+                </Text>                
+            </Box>
+        </Container>
+
+    )
+}
