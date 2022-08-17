3bea87311cebf864649ebc88c416e9d440b43682
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-08-18 00:27:11 +0300

    #kjWEB-02/AvatarImg fixed

--- WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
+++ WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
@@ -9,6 +9,7 @@ export const AvatarImage = () => {
                     boxSize='200px'
                     src='https://user-images.githubusercontent.com/44809357/184952777-451f8e3d-f73b-4474-97be-d604b5accf67.jpg'
                     alt='Kharj Memoji Photo'
+                    minW='200px'
                     />     
                 </Box>
             
