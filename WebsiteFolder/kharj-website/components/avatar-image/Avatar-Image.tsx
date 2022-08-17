53f56a5c5d5c7d62079c446a1e2bf100ed593533
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-08-18 00:33:42 +0300

    #kjWEB-02/AvatarImg updated without bg

--- WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
+++ WebsiteFolder/kharj-website/components/avatar-image/Avatar-Image.tsx
@@ -7,7 +7,7 @@ export const AvatarImage = () => {
                     <Img
                     borderRadius='full'
                     boxSize='200px'
-                    src='https://user-images.githubusercontent.com/44809357/184952777-451f8e3d-f73b-4474-97be-d604b5accf67.jpg'
+                    src='https://user-images.githubusercontent.com/44809357/185246776-9096cfb3-7abb-4af5-9cd6-109cbd6c9dcd.PNG'
                     alt='Kharj Memoji Photo'
                     minW='200px'
                     />     
