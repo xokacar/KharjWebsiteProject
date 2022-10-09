cd367a1ce19a201f4c2fd61891581a37dcc47a42
Okay Kaçar <okaykacar31@gmail.com>
2022-10-09 15:34:34 +0300

    Fix:#kjWEB-11/ Deploy2

--- WebsiteFolder/kharj-website/components/homepage-content/HomepageContent.tsx
+++ WebsiteFolder/kharj-website/components/homepage-content/HomepageContent.tsx
@@ -1,47 +1,15 @@
-import {
-	Heading,
-	Text,
-	VStack,
-	Stack,
-} from '@chakra-ui/react';
+import { Heading, Text, VStack, Stack } from '@chakra-ui/react';
 import React from 'react';
 import { AvatarImage } from '../avatar-image/Avatar-Image';
 import { Section } from '../layout/section';
-import { Bio }  from '../Bio/bio';
-import  {ILove} from '../i-love/ILove';
+import { Bio } from '../Bio/bio';
+import { ILove } from '../i-love/ILove';
 import { Contact } from '../contact/contact';
-
 
 export const HomepageContent = () => {
 	return (
 		<Section>
-		<React.Fragment>
-		<Stack
-			as='section'
-			alignItems='center'
-			direction={{ base: 'column-reverse', md: 'row' }}
-			w='full'
-			spacing={12}
-		>
-			<VStack alignItems='flex-start' w='full' spacing={3}>
-				<Stack
-					alignItems='center'
-					justifyContent={{ base: 'center', md: 'flex-start' }}
-					direction={{ base: 'column', md: 'row' }}
-					w='full'
-					spacing={3}
-				>
-					<Heading as='h1' size='lg' letterSpacing='wider'>
-						Kharj
-					</Heading>
-				</Stack>
-				<Text as='h2' lineHeight='160%'>
-					Hey I'm Kharj and I'm a software engineer. I'm currently working at a
-					startup called as Pavza a frontend engineer. I'm also a student at the
-					University of Turkish Aeronautical Association studying Computer
-					Engineer.
-				</Text>
-				<VStack alignItems='flex-start' w='full' spacing={3}>
+			<React.Fragment>
 				<Stack
 					as='section'
 					alignItems='center'
@@ -49,22 +17,46 @@ export const HomepageContent = () => {
 					w='full'
 					spacing={12}
 				>
-					</Stack>
-				</VStack>
-			</VStack>
-			<AvatarImage />
-		</Stack>
-		</React.Fragment>
-		<React.Fragment>
-			<Bio />
-		</React.Fragment>
-		<React.Fragment>
-		<ILove />
-		</React.Fragment>
-		<React.Fragment>
-			<Contact />
-		</React.Fragment>
+					<VStack alignItems='flex-start' w='full' spacing={3}>
+						<Stack
+							alignItems='center'
+							justifyContent={{ base: 'center', md: 'flex-start' }}
+							direction={{ base: 'column', md: 'row' }}
+							w='full'
+							spacing={3}
+						>
+							<Heading as='h1' size='lg' letterSpacing='wider'>
+								Kharj
+							</Heading>
+						</Stack>
+						<Text as='h2' lineHeight='160%'>
+							Hey I'm Kharj and I'm a software engineer. I'm currently working
+							at a startup called as Pavza a frontend engineer. I'm also a
+							student at the University of Turkish Aeronautical Association
+							studying Computer Engineer.
+						</Text>
+						<VStack alignItems='flex-start' w='full' spacing={3}>
+							<Stack
+								as='section'
+								alignItems='center'
+								direction={{ base: 'column-reverse', md: 'row' }}
+								w='full'
+								spacing={12}
+							></Stack>
+						</VStack>
+					</VStack>
+					<AvatarImage />
+				</Stack>
+			</React.Fragment>
+			<React.Fragment>
+				<Bio />
+			</React.Fragment>
+			<React.Fragment>
+				<ILove />
+			</React.Fragment>
+			<React.Fragment>
+				<Contact />
+			</React.Fragment>
 		</Section>
 	);
 };
-
